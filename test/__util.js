f7c9c2f24fdbbb3ded9e60ddc238df4a07de4203
Selena Vargas <smvargas@fullsail.edu>
2016-08-18 23:17:40 -0400

    finishing test

--- test/__util.js
+++ test/__util.js
@@ -1,9 +1,16 @@
 const util = require('../lib/util');
 const expect = require('chai').expect;
 
-describe('TimeTo_Go Debug Testing: ', () => {
-  it('Should Read Debug Lines String', (done) => {
+describe('TimeTo_Go Debug Test: ', () => {
+  it('Should Read Debug Line Strings', (done) => {
     expect(util.out).to.not.equal(null);
+    done();
+  });
+});
+
+describe('TimeTo_Go Time Test: ', () => {
+  it('Should Read The Time/Date Format', (done) => {
+    expect(util.time).to.not.equal(null);
     done();
   });
 });
